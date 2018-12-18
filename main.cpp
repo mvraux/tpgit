3a965435ff7b3fc4c4b4799a9b438b41b9001ea4
mvraux <mvraux@btslivh.eu>
2018-12-18 15:53:39 +0100

    added "cout << "helloworld"

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 18 décembre 2018, 15:50
+ */
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "helloworld";
+    return 0;
+}
+
